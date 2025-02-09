fefd673e6d5acda9e1ae46d01e7145a4b385e007
Kranthi Kumar Yeldi <yeldikrantthikumarr@gmail.com>
2025-02-09 15:01:09 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    data = request.get_json()
+    num1 = float(data['num1'])
+    num2 = float(data['num2'])
+    operation = data['operation']
+    
+    result = 0
+    if operation == 'add':
+        result = num1 + num2
+    elif operation == 'subtract':
+        result = num1 - num2
+    elif operation == 'multiply':
+        result = num1 * num2
+    elif operation == 'divide':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return jsonify({'error': 'Cannot divide by zero'}), 400
+            
+    return jsonify({'result': result})
+
+if __name__ == '__main__':
+    app.run(debug=True)
